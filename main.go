794ae6dda01a64465c84faf5a59c495a95a3924a
NoirJ0e <noirjoe@outlook.com>
2025-07-04 23:07:40 +1000

    feat: init tasks creation and append to slice

--- main.go
+++ main.go
@@ -1,6 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// create a struct to define a "task"
+// that should contain task content, task creation date, task id and if task is
+// a done at least
+type taskStruct struct {
+	id           string // will use uuid
+	content      string
+	createDate   time.Time
+	completeDate time.Time
+	isComplete   bool
+}
+
+func createTask(content string) taskStruct {
+	task := taskStruct{
+		id:         uuid.NewString(),
+		content:    content,
+		createDate: time.Now(),
+		isComplete: false,
+	}
+	return task
+}
+
+func addTask(tasks *[]taskStruct, task taskStruct) {
+	*tasks = append(*tasks, task)
+}
 
 func main() {
 	fmt.Println("TODO App")
--- main_test.go
+++ main_test.go
@@ -2,7 +2,57 @@ package main
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
+
+func TestCreateValidTask(t *testing.T) {
+	taskContent := "Test"
+	testTask := createTask(taskContent)
+	if testTask.content != taskContent {
+		t.Errorf("Content was not set correctly")
+	}
+
+	if testTask.isComplete != false {
+		t.Errorf("complete mark was not set correctly")
+	}
+
+	if time.Since(testTask.createDate) >= time.Second {
+		t.Errorf("initial create time was not set correctly")
+	}
+
+	if !testTask.completeDate.IsZero() {
+		t.Errorf("initial complete time was not set correctly")
+	}
+
+	_, err := uuid.Parse(testTask.id)
+	if err != nil {
+		t.Errorf("id is not a valid uuid: %v", err)
+	}
+}
+
+func TestAddTask(t *testing.T) {
+	// define a slice to include tasks first
+	var tasks []taskStruct
+
+	// create a content first
+	testTaskContent := "This a task content"
+
+	// create a task
+	testTask := createTask(testTaskContent)
+	addTask(&tasks, testTask)
+
+	// verify if the tasks slice is not null now
+	if len(tasks) == 0 {
+		t.Errorf("Failed to add task")
+	}
+
+	// test if the content in the newlly add tasks is same as pre-defined
+	if tasks[0].content != testTaskContent {
+		t.Errorf("Failed to create task: incorrect task content")
+	}
+}
 
 func TestRecordExist(t *testing.T) {
 }
